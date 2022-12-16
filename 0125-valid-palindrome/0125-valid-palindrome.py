6b998127196181d2103cd6e9d43caa5c1caf81d9
Ketema Girma <ketemagirma15@gmail.com>
2022-12-16 19:17:30 +0300

    Time: 55 ms (85.09%), Space: 14.6 MB (50.50%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        if len(s) == 0: return True
+        left, right = 0, len(s) - 1
+        while right > left:
+            if not s[right].isalnum():
+                right -= 1
+                continue
+            if not s[left].isalnum():
+                left += 1
+                continue
+            if s[left].lower() != s[right].lower():
+                return False
+            left += 1
+            right -= 1
+        return True
+    """
+                        l
+    a m a n a p l a n a c  a n a l p a n a m a
+                        r
+    """
