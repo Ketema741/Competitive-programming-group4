563c254da77f7a2ec013884a5061eb76bdf1ba8f
Ketema Girma <ketemagirma15@gmail.com>
2022-12-26 15:28:12 +0300

    wrap word

---
+++ pythonTrack/wrapWords.py
@@ -0,0 +1,12 @@
+import textwrap
+
+def wrap(string, max_width):
+    wrapper = textwrap.TextWrapper(width=max_width)
+    
+    word_list = wrapper.wrap(string)
+    return "\n".join(word_list)
+
+if __name__ == '__main__':
+    string, max_width = input(), int(input())
+    result = wrap(string, max_width)
+    print(result)
