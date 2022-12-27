6bcf94ec2087fa42a2d6a9abf73547323fad4a5b
Ketema Girma <ketemagirma15@gmail.com>
2022-12-27 19:35:50 +0300

    Update 0043-multiply-strings.py

--- 0043-multiply-strings/0043-multiply-strings.py
+++ 0043-multiply-strings/0043-multiply-strings.py
@@ -1,4 +1,16 @@
 class Solution:
     def multiply(self, num1: str, num2: str) -> str:
-        return str(int(num1) * int(num2))
+        res = ''
+        int_num_1 = 0
+        int_num_2 = 0
+        hash_map = {"0":0, "1":1, "2":2,"3":3, "4":4, "5":5,"6":6, "7":7, "8":8, "9":9}
+        num1 = reversed(num1)
+        num2 = reversed(num2)
+        for indx, num in enumerate(num1):
+            int_num_1 += hash_map[num]* 10**indx
+            
+        for indx, num in enumerate(num2):
+            int_num_2 += hash_map[num]* 10**indx
+       
+        return str(int_num_1 * int_num_2)
         
