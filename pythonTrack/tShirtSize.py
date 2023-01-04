efc60020a7960d230c27fadf153dcb6692be67ac
Ketema Girma <ketemagirma15@gmail.com>
2023-01-04 17:52:04 +0300

    t-shirt size

---
+++ pythonTrack/tShirtSize.py
@@ -0,0 +1,28 @@
+def calculate_shirt_size(shirt):
+    hash_map = {'S':1, 'M':2, 'L':3, 'X':4}
+    shirt_size = 0
+    if len(shirt) > 1:
+        for indx in range(len(shirt)-1):
+            shirt_size += hash_map[shirt[indx]]
+        if shirt[-1] == 'L':
+            shirt_size += hash_map['L']
+        else:
+            shirt_size = hash_map['S'] - shirt_size
+    else:
+        shirt_size += hash_map[shirt]
+    return shirt_size
+
+N = int(input())
+res = []
+for inputs in range(N):
+    first_tshirt, second_tshirt= input().split()
+    size1 = calculate_shirt_size(first_tshirt)
+    size2 = calculate_shirt_size(second_tshirt)
+    if size1 < size2:
+        res.append('<')
+    elif size1 > size2:
+        res.append('>')
+    else:
+        res.append('=')
+for operator in res:
+    print(operator)
