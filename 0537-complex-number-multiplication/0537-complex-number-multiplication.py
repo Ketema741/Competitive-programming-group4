3a560c195cc83f04350ee35ebf92ca04f84d857e
Ketema Girma <ketemagirma15@gmail.com>
2023-01-08 15:28:35 +0300

    Time: 36 ms (71.38%), Space: 13.9 MB (15.11%) - LeetHub

--- 0537-complex-number-multiplication/0537-complex-number-multiplication.py
+++ 0537-complex-number-multiplication/0537-complex-number-multiplication.py
@@ -2,10 +2,9 @@ class Solution:
     def complexNumberMultiply(self, num1: str, num2: str) -> str:
         r1, i1 = map(int, num1.rstrip('i').split('+'))
         r2, i2 = map(int, num2.rstrip('i').split('+'))
-        print(r1, i1)
+    
         real = r1 * r2 - i1 * i2
         imag = r1 * i2 + r2 * i1
-        
         
         return f'{real}+{imag}i'  
         
