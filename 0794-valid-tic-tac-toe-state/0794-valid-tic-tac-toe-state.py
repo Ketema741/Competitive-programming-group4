aea1ac2782b5f9643b2faf70041ed5dd6344b25b
Ketema Girma <ketemagirma15@gmail.com>
2023-01-08 14:00:10 +0300

    Time: 35 ms (82.41%), Space: 13.9 MB (92.07%) - LeetHub

--- 0794-valid-tic-tac-toe-state/0794-valid-tic-tac-toe-state.py
+++ 0794-valid-tic-tac-toe-state/0794-valid-tic-tac-toe-state.py
@@ -1,39 +1,42 @@
 class Solution:
-    def check_win_positions(self, board, player):
-        #Check the rows
-        for i in range(len(board)):
-            if board[i][0] == board[i][1] == board[i][2] == player:
-                return True                        
+    def has_won(self, board, player):
+        # Check rows
+        for row in board:
+            if row[0] == player and row[1] == player and row[2] == player:
+                return True
 
-        #Check the columns
-        for i in range(len(board)):
-            if board[0][i] == board[1][i] == board[2][i] == player:
-                return True 
-										
-        #Check the diagonals
-        if board[0][0] == board[1][1] == board[2][2]  == player or \
-            board[0][2] == board[1][1] == board[2][0] == player:
+        # Check columns
+        for col in range(3):
+            if board[0][col] == player and board[1][col] == player and board[2][col] == player:
+                return True
+
+        # Check diagonals
+        if board[0][0] == player and board[1][1] == player and board[2][2] == player:
             return True
+        if board[0][2] == player and board[1][1] == player and board[2][0] == player:
+            return True
+
         return False
-        
-    def validTicTacToe(self, board: List[str]) -> bool:
-        x_count, o_count = 0, 0
-        for i in range(len(board)):
-            for j in range(len(board[0])):
-                if board[i][j] == "X":
+    def validTicTacToe(self, board):
+        x_count = 0
+        o_count = 0
+        for row in board:
+            for c in row:
+                if c == 'X':
                     x_count += 1
-                elif  board[i][j] == "O":
+                elif c == 'O':
                     o_count += 1
-										
-        if o_count > x_count or x_count - o_count > 1:
+
+        # Check if the number of Xs and Os is valid
+        if x_count < o_count or x_count > o_count + 1:
             return False
-        
-        if self.check_win_positions(board, 'O'):
-            if self.check_win_positions(board, 'X'):
-                return False
-            return o_count == x_count
-        
-        if self.check_win_positions(board, 'X') and x_count != o_count + 1:
+
+        # Check if X has won
+        if x_count - o_count == 0 and self.has_won(board, 'X'):
+            return False
+
+        # Check if O has won
+        if x_count - o_count == 1 and self.has_won(board, 'O'):
             return False
 
         return True
