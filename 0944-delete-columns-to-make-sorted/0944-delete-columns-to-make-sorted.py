4a2ce7404fffe85e09e721c9b9060d8c0abc66ea
Ketema Girma <ketemagirma15@gmail.com>
2023-01-12 20:46:51 +0300

    Time: 149 ms (90.02%), Space: 14.5 MB (92.08%) - LeetHub

--- 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.py
+++ 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.py
@@ -1,11 +1,9 @@
 class Solution:
     def minDeletionSize(self, strs: List[str]) -> int:
-        hash_map = defaultdict(list)
         count = 0
         for col_idx in range(len(strs[0])):
-             for row_idx in range(len(strs)):
-                    hash_map[col_idx]. append(strs[row_idx][col_idx])
-        for strings in hash_map.values():
-            if strings != sorted(strings):
-                count += 1
+             for row_idx in range(1, len(strs)):
+                if  strs[row_idx - 1][col_idx] > strs[row_idx][col_idx]:
+                    count += 1
+                    break
         return count
