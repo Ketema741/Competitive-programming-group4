d94d57c723a9c567ebb4bbdf44a80eb6ea1b144f
Ketema Girma <ketemagirma15@gmail.com>
2023-01-13 14:54:22 +0300

    weekly contest

--- contest/contest1/traffic.py
+++ contest/contest1/traffic.py
@@ -4,12 +4,4 @@ for i in range(num_testcase):
     N = input().split()
     string = input()
     for i in range(len(string)):
-        
-        
-
-"""
-    5 r
-    r g g r y
-    0 1 2 3 4
-"""
-        
+        # not finished yet 😊
