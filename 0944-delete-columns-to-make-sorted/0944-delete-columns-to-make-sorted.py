0402859c74723ebefe65a62e96b1afaa20b5ec70
Ketema Girma <ketemagirma15@gmail.com>
2023-01-13 13:27:15 +0300

    Time: 143 ms (92.06%), Space: 14.6 MB (92.03%) - LeetHub

--- 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.py
+++ 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.py
@@ -1,9 +1,13 @@
 class Solution:
     def minDeletionSize(self, strs: List[str]) -> int:
-        count = 0
+        deleted_cols = 0
+        
+        # traversing column wise          
         for col_idx in range(len(strs[0])):
              for row_idx in range(1, len(strs)):
+                
+                # for a given column check if the previous char is greate than the current  which means unsorted
                 if  strs[row_idx - 1][col_idx] > strs[row_idx][col_idx]:
-                    count += 1
+                    deleted_cols += 1
                     break
-        return count
+        return deleted_cols
