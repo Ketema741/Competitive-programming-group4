e2315b45b84eb4748ad19a8e9de32a0dd4c54181
Ketema Girma <ketemagirma15@gmail.com>
2023-01-18 14:53:08 +0300

    Time: 103 ms (40.42%), Space: 13.9 MB (32.26%) - LeetHub

--- 0766-toeplitz-matrix/0766-toeplitz-matrix.py
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.py
@@ -5,12 +5,19 @@ class Solution:
         
         # Iterate through each element of the input matrix
         for row in range(rows):
-             for col in range(cols):
-                    sub_col = col
-                    sub_row = row
-                    while  sub_row + 1 < rows and sub_col +1 < cols:
-                        if matrix[sub_row + 1][sub_col + 1] != matrix[row][col]:
-                            return False
-                        sub_row += 1
-                        sub_col += 1
+            for col in range(cols):
+                # variables to keep track of the current sub-matrix
+                sub_col = col
+                sub_row = row
+                # check diagonally if current sub-matrix is still within the bounds
+                while  sub_row + 1 < rows and sub_col + 1 < cols:
+                
+                    # compare the value of the current sub-matrix with the value of the starting element
+                    if matrix[sub_row + 1][sub_col + 1] != matrix[row][col]:
+                        # If not, return False as the input matrix is not a Toeplitz matrix
+                        return False
+                    # Move to the next sub-matrix
+                    sub_row += 1
+                    sub_col += 1
+        # If all sub-matrices have been checked and none have returned false,  matrix is a Toeplitz 
         return True
