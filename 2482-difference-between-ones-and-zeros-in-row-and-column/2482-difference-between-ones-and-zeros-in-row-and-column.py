bc4db3fc4680b3fe8539800dde5c3a3e0c0a2ef7
Ketema Girma <ketemagirma15@gmail.com>
2023-01-22 10:51:26 +0300

    Time: 3548 ms (47.94%), Space: 84.4 MB (5.16%) - LeetHub

--- 2482-difference-between-ones-and-zeros-in-row-and-column/2482-difference-between-ones-and-zeros-in-row-and-column.py
+++ 2482-difference-between-ones-and-zeros-in-row-and-column/2482-difference-between-ones-and-zeros-in-row-and-column.py
@@ -1,8 +1,15 @@
 class Solution:
     def onesMinusZeros(self, grid: List[List[int]]) -> List[List[int]]:
+        # Get the dimensions of the grid
         rows, cols = len(grid), len(grid[0])
+        
+        # Initialize the diff grid
         diff = [[0 for _ in range(cols)] for _ in range(rows)]
+        
+        # Create a hash_map to store count of ones and zeros in rows and columns
         hash_map = defaultdict(int)
+        
+        # Count the number of ones and zeros in each row and column
         for row in range(rows):
             for col in range(cols):
                 if grid[row][col] == 1:
@@ -12,6 +19,8 @@ class Solution:
                 else:
                     hash_map[(row, 'r0')] += 1
                     hash_map[(col, 'c0')] += 1
+        
+        # Compute the diff matrix
         for row in range(rows):
             for col in range(cols):
                 diff[row][col] = hash_map[(row, 'r1')] + hash_map[(col, 'c1')] - hash_map[(row, 'r0')] - hash_map[(col, 'c0')]
