38d5ce0ceff714711a074c0789c00bb2f7a3a0d9
Ketema Girma <ketemagirma15@gmail.com>
2023-01-22 10:12:16 +0300

    Time: 59 ms (42.46%), Space: 13.9 MB (52.76%) - LeetHub

--- 1222-queens-that-can-attack-the-king/1222-queens-that-can-attack-the-king.py
+++ 1222-queens-that-can-attack-the-king/1222-queens-that-can-attack-the-king.py
@@ -1,44 +1,57 @@
 class Solution:
     def queensAttacktheKing(self, queens: List[List[int]], king: List[int]) -> List[List[int]]:
+        # Define chessboard dimensions
         rows, cols = 8, 8
+        
+        # King's position
         row, col = king
+        
+        # Store attacking queens
         res = []
 
+        # Check for queens attacking from top
         for x in range(row, -1, -1):
             if [x, col] in queens:
                 res.append([x, col])
                 break
 
+        # Check for queens attacking from bottom
         for x in range(row, rows):
             if [x, col] in queens:
                 res.append([x, col])
                 break
                 
+        # Check for queens attacking from left
         for y in range(col, -1, -1):
             if [row, y] in queens:
                 res.append([row, y])
                 break
         
+        # Check for queens attacking from right
         for y in range(col, cols):
             if [row, y] in queens:
                 res.append([row, y])
                 break
             
+        # Check for queens attacking from top-left diagonal
         for x, y in zip(range(row, -1, -1), range(col, -1, -1)):
             if [x, y] in queens:
                 res.append([x, y])
                 break
         
+        # Check for queens attacking from bottom-right diagonal
         for x, y in zip(range(row, rows), range(col, cols)):
             if [x, y] in queens:
                 res.append([x, y])
                 break
             
+        # Check for queens attacking from top-right diagonal
         for x, y in zip(range(row, -1, -1), range(col, cols)):
             if [x, y] in queens:
                 res.append([x, y])
                 break
         
+        # Check for queens attacking from bottom-left diagonal
         for x, y in zip(range(row, rows), range(col, -1, -1)):
             if [x, y] in queens:
                 res.append([x, y])
