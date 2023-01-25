522cc30efc54f0010ba353c037785df20b917a4c
Ketema Girma <ketemagirma15@gmail.com>
2023-01-25 18:39:08 +0300

    Time: 44 ms (66.38%), Space: 13.9 MB (61.28%) - LeetHub

--- 0763-partition-labels/0763-partition-labels.py
+++ 0763-partition-labels/0763-partition-labels.py
@@ -2,17 +2,19 @@ class Solution:
     def partitionLabels(self, s: str) -> List[int]:
         hash_map = defaultdict()
         res = []
+        
         for indx, char in enumerate(s):
             hash_map[char] = indx
 
-        max_ = hash_map[s[0]]
-        end = 0
-        print(hash_map)
-        for indx, char in enumerate(s):
-            if hash_map[char] > max_:
-                max_ = hash_map[char]
-            if max_ == indx:
-                max_ += 1
-                res.append(max_ - end)
-                end = max_
+        left, max_len = 0, hash_map[s[0]]
+
+        for right, char in enumerate(s):
+            if hash_map[char] > max_len:
+                max_len = hash_map[char]
+                
+            if max_len == right:
+                max_len += 1
+                res.append(max_len - left)
+                left = max_len
+                
         return res
