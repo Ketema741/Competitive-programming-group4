3b6ec97856ada35b41d4773a0d9ff05aa70d9383
Ketema Girma <ketemagirma15@gmail.com>
2023-01-30 21:42:35 +0300

    Time: 69 ms (93.91%), Space: 14.7 MB (90.36%) - LeetHub

--- 0867-transpose-matrix/0867-transpose-matrix.py
+++ 0867-transpose-matrix/0867-transpose-matrix.py
@@ -1,14 +1,10 @@
 class Solution:
     def transpose(self, matrix: List[List[int]]) -> List[List[int]]:
         R, C = len(matrix), len(matrix[0])
-        
         res = [[None] * R for _ in range(C)]
+        
         for r, row in enumerate(matrix):
             for c, val in enumerate(row):
                 res[c][r] = val
+                
         return res
-
-
-        """
-        
-        """
