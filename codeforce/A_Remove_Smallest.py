95da02667d51f4c6c7f8579d4e423d69352215e1
Ketema Girma <ketemagirma15@gmail.com>
2023-02-20 14:24:23 +0300

    remove the smallest

--- codeforce/A_Remove_Smallest.py
+++ codeforce/A_Remove_Smallest.py
@@ -2,24 +2,18 @@ test_len = int(input())
 
 for _ in range(test_len):
     n = int(input())
-    nums = list(map(int, input().split()))
+    nums = sorted(list(map(int, input().split())))
     if n == 1:
         print("YES")
     elif n == 2:
         print("YES" if abs(nums[0] - nums[1]) <= 1 else "NO")
     else:
-        nums.sort()
-        for i in range(1, n-1):
-            if nums[i] - nums[i-1] > 1:
-                print("NO")
+        valid = True
+        for right in range(1, n):
+            if abs(nums[right] - nums[right - 1]) > 1:
+                valid = False
                 break
-            else:
-                print("YES")
-
-    """  
-        1   2   3   4 
-        
-        5   6   7   8
-        
-        5   6   7   8
-    """ 
+        if valid:  
+            print("YES")
+        else:
+            print("NO")
