e684a50d7270d6aec042cbd1623de875523eb1d8
Ketema Girma <ketemagirma15@gmail.com>
2023-02-28 15:37:22 +0300

    Time: 214 ms (32.06%), Space: 43.4 MB (5.49%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -1,7 +1,15 @@
 class Solution:
+    def swapper(self, s, left, right):
+        if left >= right:
+            return s
+        
+        s[left], s[right] = s[right], s[left]
+        
+        return self.swapper(s, left + 1, right - 1)
+    
     def reverseString(self, s: List[str]) -> None:
         left, right = 0, len(s) - 1
-        while right > left :
-            s[left], s[right] = s[right], s[left]
-            right -= 1
-            left += 1
+        
+        res = self.swapper(s, left, right)
+        
+        return res
