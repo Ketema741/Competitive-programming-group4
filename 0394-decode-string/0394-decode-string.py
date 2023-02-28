e8fa0e8f3e629ce3e74de65cf94e1a43d544b36d
Ketema Girma <ketemagirma15@gmail.com>
2023-02-28 17:43:08 +0300

    Time: 33 ms (56.46%), Space: 13.8 MB (97.65%) - LeetHub

---
+++ 0394-decode-string/0394-decode-string.py
@@ -0,0 +1,22 @@
+class Solution:
+    def decodeString(self, s: str) -> str:
+        stack, res = [], []
+        
+        for i in range(len(s)):
+            if s[i] != ']':
+                stack.append(s[i])
+            else:
+                string = ''
+                
+                while stack[-1] !='[':
+                    string = stack.pop() + string
+                
+                stack.pop()
+                k = ''
+                
+                while stack and stack[-1].isdigit():
+                    k = stack.pop() + k
+                    
+                stack.append(int(k)*string)
+                
+        return '' if stack[0].isdigit() else ''.join(stack) 
