3506768ee16302750774b7efc3e5ed21c161c710
Ketema Girma <ketemagirma15@gmail.com>
2023-03-01 11:21:54 +0300

    Time: 59 ms (36.59%), Space: 13.9 MB (53.64%) - LeetHub

---
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.py
@@ -0,0 +1,13 @@
+class Solution:
+    def timeRequiredToBuy(self, tickets: List[int], k: int) -> int:
+        times = 0
+        
+        while tickets[k] != 0:
+            for i in range(len(tickets)):
+                if tickets[k] == 0:
+                    break
+                    
+                if tickets[i] != 0:
+                    tickets[i] -= 1
+                    times += 1
+        return times
