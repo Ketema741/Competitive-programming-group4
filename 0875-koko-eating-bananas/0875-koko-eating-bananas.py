c4851d375a14d2d6c7a3359385c10c29486176cd
Ketema Girma <ketemagirma15@gmail.com>
2023-03-04 03:24:17 +0300

    Time: 457 ms (82.23%), Space: 15.5 MB (16.71%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.py
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.py
@@ -4,13 +4,17 @@ class Solution:
         def canEatAll(k, hrs_left):
             for pile in piles:
                 hrs_left -= math.ceil(pile/k)
+            
             return hrs_left >= 0
 
-        l, r = 1, max(piles)
-        while l < r:
-            mid = (l+r)//2
+        left, right = 1, max(piles)
+        
+        while left < right:
+            mid = (left + right) // 2
+            
             if canEatAll(mid, h):
-                r = mid
+                right = mid
             else:
-                l = mid + 1
-        return l
+                left = mid + 1
+                
+        return left
