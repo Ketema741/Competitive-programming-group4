e3ad6eedac76687ec3ca1944eb0a0efdb68e5749
Ketema Girma <ketemagirma15@gmail.com>
2023-03-13 05:50:47 +0300

    Time: 29 ms (93.28%), Space: 14 MB (77.66%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,16 +1,19 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         res = []
-        def backtrack(indx, curnt_subset):
-            res.append(curnt_subset[:])
-            
-            for i in range (indx, n):
-                curnt_subset.append(nums[i])
-                backtrack(i+1, curnt_subset)
-                curnt_subset.pop()
         
-        res, n = [], len(nums)
+        def backtrack(indx, subset):
+            if indx == len(nums):
+                res.append(subset[:])
+                return
+            
+            res.append(subset[:])
+            
+            for i in range(indx, len(nums)):
+                subset.append(nums[i])      # choose
+                backtrack(i+1, subset)      # explore
+                subset.pop()                # unchoose
+                
         backtrack(0, [])
         
-            
         return res
