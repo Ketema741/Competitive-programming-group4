d212c951d91f376dc617be0d39508e7bb519ff4b
Ketema Girma <ketemagirma15@gmail.com>
2023-03-14 21:27:11 +0300

    Time: 240 ms (81.17%), Space: 15.5 MB (59.51%) - LeetHub

--- 0704-binary-search/0704-binary-search.py
+++ 0704-binary-search/0704-binary-search.py
@@ -3,6 +3,7 @@ class Solution:
         left, right = 0, len(nums) - 1
         while left <= right:
             mid = (left + right ) // 2
+            
             if nums[mid] == target:
                 return mid
             
