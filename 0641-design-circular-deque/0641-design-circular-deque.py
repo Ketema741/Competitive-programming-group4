50ad2c8fe507836a82e53bf910638b5e21520f5d
Ketema Girma <ketemagirma15@gmail.com>
2023-03-18 20:21:06 +0300

    Time: 133 ms (5.09%), Space: 14.6 MB (46.69%) - LeetHub

--- 0641-design-circular-deque/0641-design-circular-deque.py
+++ 0641-design-circular-deque/0641-design-circular-deque.py
@@ -4,10 +4,7 @@ class MyCircularDeque:
         self.k = k
         self.stack1 = []
         self.stack2 = []
-        self.l = 0
-        self.r = 0
-        
-
+       
     def insertFront(self, value: int) -> bool:
         if len(self.stack1)< self.k:
             for i in self.stack1[::-1]:
@@ -18,18 +15,17 @@ class MyCircularDeque:
                 self.stack1.append(i)
             self.stack2.clear()
             return True
-        else:
-            return False
+
+        return False
         
 
     def insertLast(self, value: int) -> bool:
         if len(self.stack1) < self.k:
             self.stack1.append(value)
             return True
-        else:
-            return False
-        
 
+        return False
+        
 
     def deleteFront(self) -> bool:
         if len(self.stack1) > 0:
@@ -41,48 +37,34 @@ class MyCircularDeque:
                 self.stack1.append(i)
             self.stack2.clear()
             return True
-        else:
-            return False
+
+        return False
         
 
     def deleteLast(self) -> bool:
         if len(self.stack1) != 0:     
             self.stack1.pop()
             return True
-        else:
-            return False
+        
+        return False
         
 
     def getFront(self) -> int:
         if len(self.stack1) !=0:
             return self.stack1[0]
-        else:
-            return -1
+        return -1
         
 
     def getRear(self) -> int:
         if len(self.stack1) != 0:
             return self.stack1[-1]
-        else:
-            return -1
+        return -1
         
 
     def isEmpty(self) -> bool:
-        return True if len(self.stack1)==0 else False
+        return len(self.stack1) == 0
         
 
     def isFull(self) -> bool:
-        return True if len(self.stack1)== self.k else False
+        return len(self.stack1) == self.k
         
-
-
-# Your MyCircularDeque object will be instantiated and called as such:
-# obj = MyCircularDeque(k)
-# param_1 = obj.insertFront(value)
-# param_2 = obj.insertLast(value)
-# param_3 = obj.deleteFront()
-# param_4 = obj.deleteLast()
-# param_5 = obj.getFront()
-# param_6 = obj.getRear()
-# param_7 = obj.isEmpty()
-# param_8 = obj.isFull()
