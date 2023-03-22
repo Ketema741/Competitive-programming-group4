417e8202a3574a956eadf78c2d18af96d8189677
Ketema Girma <ketemagirma15@gmail.com>
2023-03-22 20:48:44 +0300

    Time: 82 ms (79.06%), Space: 15.4 MB (72.13%) - LeetHub

--- 0617-merge-two-binary-trees/0617-merge-two-binary-trees.py
+++ 0617-merge-two-binary-trees/0617-merge-two-binary-trees.py
@@ -16,6 +16,7 @@ class Solution:
                 return root1
            
             root1.val += root2.val
+            
             root1.left = merge(root1.left, root2.left)
             root1.right = merge(root1.right, root2.right)
             
