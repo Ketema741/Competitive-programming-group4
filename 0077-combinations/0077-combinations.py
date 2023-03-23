582f7b2be913aa6972d8c2c7634c4613b086e153
Ketema Girma <ketemagirma15@gmail.com>
2023-03-23 15:41:16 +0300

    Time: 421 ms (50.78%), Space: 15.9 MB (43.61%) - LeetHub

--- 0077-combinations/0077-combinations.py
+++ 0077-combinations/0077-combinations.py
@@ -8,10 +8,32 @@ class Solution:
             
             if start > n:
                 return
-
-            backtrack(start+1, comb + [start])
-            backtrack(start+1, comb)
-
+            
+            comb.append(start) # choose
+            backtrack(start + 1, comb ) #  then explore
+            
+            comb.pop() # unchoose
+            backtrack(start + 1, comb) #  then explore
 
         backtrack(1, [])
         return res
+    
+    
+    
+    """
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        res = []
+        def backtrack(start, comb):
+            if len(comb) == k:
+                res.append(comb.copy())
+                return
+            
+            for i in range(start, n+1):
+                comb.append(i)
+                backtrack(i+1, comb)
+                comb.pop()
+                
+        backtrack(1, [])
+        return res
+    
+    """
