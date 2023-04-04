7071308a2c5ebd42c7c84c105eaa5e9780de06a9
Ketema Girma <ketemagirma15@gmail.com>
2023-04-04 17:44:59 +0300

    Time: 617 ms (74.08%), Space: 17.4 MB (35.29%) - LeetHub

--- 0318-maximum-product-of-word-lengths/0318-maximum-product-of-word-lengths.py
+++ 0318-maximum-product-of-word-lengths/0318-maximum-product-of-word-lengths.py
@@ -6,16 +6,15 @@ class Solution:
         for word in words:
             mask = 0
             for char in word:
-                bit=ord(char)-ord('a')
+                bit = ord(char) - ord('a')
                 mask |= (1<<bit)
                 
             masks.append(mask)
             
         res = 0
         for i in range(n):
-            for j in range(n):
-                if i != j:
-                    if (masks[i] & masks[j]) == 0:
-                        res = max(res, len(words[i])*len(words[j]))
+            for j in range(i+1, n):
+                if (masks[i] & masks[j]) == 0:
+                    res = max(res, len(words[i])*len(words[j]))
                         
         return res
