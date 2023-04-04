db6610b1998d77384b0207866a170e39324124d9
Ketema Girma <kgirma363@gmail.com>
2023-04-04 11:31:04 +0300

    cirnos perfect bitmasks classroom

--- codeforce/progres sheet/A_Cirno_s_Perfect_Bitmasks_Classroom.py
+++ codeforce/progres sheet/A_Cirno_s_Perfect_Bitmasks_Classroom.py
@@ -3,10 +3,21 @@ test_len = int(input())
 for i in range(test_len):
     num = int(input())
 
-    min_pos = num&-num
+    bin_ = bin(num)
+    len_ = right = len(bin_) - 1
+    min_positive = 1
+    
+    while bin_[right] != 'b':
+        if bin_[right] == '1':
+            break
+        right -= 1 
+
+    if bin_[right] == '1':    
+        min_positive = 2**(len_ - right)
+
     while True:
-        if (num^min_pos) > 0 and (num&min_pos) > 0:
-            print(min_pos)
+        if (num^min_positive) > 0 and (num&min_positive) > 0:
+            print(min_positive)
             break
         else:
-            min_pos += 1
+            min_positive += 1
