b0d93d863197643240c17227a61e263dde2bd511
Ketema Girma <ketemagirma15@gmail.com>
2023-04-04 21:49:21 +0300

    Time: 467 ms (61.54%), Space: 13.9 MB (45.70%) - LeetHub

---
+++ 2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets.py
@@ -0,0 +1,24 @@
+class Solution:
+    def countMaxOrSubsets(self, nums: List[int]) -> int:
+        target = 0
+        count = 0
+        
+        for num in nums:
+            target |= num
+            
+        def backtrack(OR, index):
+            nonlocal count
+            if OR == target:
+                count += 1
+                
+            if index == len(nums):
+                return
+            
+            for i in range(index, len(nums)):
+                new_OR = OR | nums[i]
+                backtrack(new_OR, i + 1)
+                
+        for i in range(len(nums)):
+            backtrack(nums[i], i + 1)
+            
+        return count
