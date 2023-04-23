193f456f954f3a0824ed7bbea23ae85952898658
Ketema Girma <ketemagirma15@gmail.com>
2023-04-23 16:22:49 +0300

    Time: 85 ms (13.30%), Space: 14.1 MB (30.13%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.py
+++ 0733-flood-fill/0733-flood-fill.py
@@ -1,19 +1,20 @@
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, newColor: int) -> List[List[int]]:
-        visited = set()
+        
         directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
         
         def inbound(row, col):
             return 0 <= row < len(image) and 0 <= col < len(image[0])
         
         def dfs(row, col, color):
-            if not inbound(row, col) or image[row][col] != color:
-                return
-            
             image[row][col] = newColor
             
             for row_change, col_change in directions:
-                dfs(row + row_change, col + col_change, color)
+                new_row = row + row_change
+                new_col = col + col_change
+
+                if inbound(new_row, new_col) and image[new_row][new_col] == color:
+                    dfs(new_row, new_col, color)
         
         color = image[sr][sc]
         
