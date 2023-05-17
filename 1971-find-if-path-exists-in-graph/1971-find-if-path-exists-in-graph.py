45d8e0d6fe4ae37c093bd4b377cd1c0a04a814a7
Ketema Girma <ketemagirma15@gmail.com>
2023-05-17 16:35:42 +0300

    Time: 1930 ms (65.22%), Space: 105.7 MB (84.17%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
@@ -1,25 +1,30 @@
+class UnionFind:
+    def __init__(self, n):
+        self.parent = [i for i in range(n)]
+        self.rank = [1] * n
+
+    def find(self, x):
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+
+    def union(self, x, y):
+        parent_of_x, parent_of_y = self.find(x), self.find(y)
+
+        if parent_of_x != parent_of_y:
+            if self.rank[parent_of_x] >= self.rank[parent_of_y]:
+                self.parent[parent_of_y] = parent_of_x
+                self.rank[parent_of_x] += self.rank[parent_of_y]
+            elif self.rank[parent_of_x] < self.rank[parent_of_y]:
+                self.parent[parent_of_x] = parent_of_y
+                self.rank[parent_of_y] += self.rank[parent_of_x]
+            
+
 class Solution:
     def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
-        
-        graph = defaultdict(list)
-        def dfs(node, visited):
-            if node == destination:
-                return True
-            
-            visited.add(node)
-            for neighbour in graph[node]:
-                if neighbour not in visited:
-                    found = dfs(neighbour, visited)
-                    if found:
-                        return True
-            return False
-        
-        
-        
-        for v1, v2 in edges:
-            graph[v1].append(v2)
-            graph[v2].append(v1)
-            
-        visited = set()
-        
-        return dfs(source, visited)
+        uf = UnionFind(n)
+
+        for a, b in edges:
+            uf.union(a, b)
+
+        return uf.find(source) == uf.find(destination)
