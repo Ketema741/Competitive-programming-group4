0457acbc834fb1562379468ecdb1ac35bd9258da
Ketema Girma <ketemagirma15@gmail.com>
2023-05-26 19:12:09 +0300

    Time: 47 ms (29.97%), Space: 16.5 MB (13.07%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.py
@@ -0,0 +1,18 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        n = len(nums)
+        
+        memo = defaultdict(int)
+        
+        def dp(n):
+            
+            if n >= len(nums):
+                return 0
+            if n in memo:
+                return memo[n]
+            
+            money = max(nums[n] + dp(n+2), dp(n+1))
+            memo[n] = money
+            
+            return memo[n]
+        return dp(0)
