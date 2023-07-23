2c06834dcb3d21be81dd64ec29830740c400bdf6
Ketema Girma <ketemagirma15@gmail.com>
2023-07-23 17:53:30 +0300

    Time: 86 ms (6.95%), Space: 18.7 MB (9.25%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
@@ -1,19 +1,15 @@
-from typing import List
-from collections import defaultdict
-
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        memo = defaultdict(int)
-
-        def dp(i):
-            if i >= len(cost):
-                return 0
-
-            if memo[i] != 0:
-                return memo[i]
-
-            memo[i] = cost[i] + min(dp(i+1), dp(i+2))
-
-            return memo[i]
-
-        return min(dp(0), dp(1))
+        hash_map = defaultdict(int)
+        n = len(cost)
+        
+        def dp(n):
+            if n < 2:
+                return cost[n]
+            
+            if n not in hash_map:
+                hash_map[n] = cost[n] + min(dp(n - 1), dp(n - 2))
+                
+            return hash_map[n]
+        
+        return min(dp(n-1), dp(n-2))
