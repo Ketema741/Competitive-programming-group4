d78f1ffcfd5382395e178eb27e3aaeeb66e1fb1c
Ketema Girma <ketemagirma15@gmail.com>
2023-08-06 01:43:50 +0300

    Time: 36 ms (91.70%), Space: 16.3 MB (70.71%) - LeetHub

---
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.py
@@ -0,0 +1,21 @@
+from collections import deque
+
+class MyStack:
+
+    def __init__(self):
+        self.queue = deque()
+
+    def push(self, x: int) -> None:
+        self.queue.append(x)
+        # Rearrange the elements so that the recently pushed element is at the front
+        for _ in range(len(self.queue) - 1):
+            self.queue.append(self.queue.popleft())
+
+    def pop(self) -> int:
+        return self.queue.popleft()
+
+    def top(self) -> int:
+        return self.queue[0]
+
+    def empty(self) -> bool:
+        return not self.queue
