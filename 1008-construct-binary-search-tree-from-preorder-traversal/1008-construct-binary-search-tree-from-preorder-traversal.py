e2b607901114cc5380d9da850c14f672b50ef964
Ketema Girma <ketema.girma@a2sv.org>
2023-09-18 11:15:22 +0300

    Time: 46 ms (46.25%), Space: 16.3 MB (65.43%) - LeetHub

--- 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.py
+++ 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.py
@@ -5,23 +5,45 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def bfs(self, preorder, size):
-        if not preorder:
-            return None
-            
-        end = 1
-        while end < size and preorder[0] > preorder[end]:
-                end += 1
-            
-        left_subtree = preorder[1:end]
-        right_subtree = preorder[end:]
-            
-        root = TreeNode(preorder[0])
-            
-        root.left = self.bfs(left_subtree, len(left_subtree))
-        root.right = self.bfs(right_subtree, len(right_subtree))
-        return root
-        
     def bstFromPreorder(self, preorder: List[int]) -> Optional[TreeNode]:
+        root = TreeNode(preorder[0])
+        stack = [root]
+        
+        for value in preorder[1:]:
+            node = TreeNode(value)
+            
+            if value < stack[-1].val:
+                stack[-1].left = node 
+                stack.append(stack[-1].left)
+            else:
+                while stack and value > stack[-1].val:
+                    last = stack.pop()
+                last.right = node
+                stack.append(last.right)
+    
+    
+        return root
+    
+    
+    
+    """
+        def bfs(preorder, size):
+            if not preorder:
+                return None
+
+            end = 1
+            while end < size and preorder[0] > preorder[end]:
+                    end += 1
+
+            left_subtree = preorder[1:end]
+            right_subtree = preorder[end:]
+
+            root = TreeNode(preorder[0])
+
+            root.left = bfs(left_subtree, len(left_subtree))
+            root.right = bfs(right_subtree, len(right_subtree))
+            return root
                 
-        return self.bfs(preorder, len(preorder))
+        return bfs(preorder, len(preorder))
+        
+        """
