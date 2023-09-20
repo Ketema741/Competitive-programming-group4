fc8ec8f22e485fbd2e5443cf916fba022480c2ac
Ketema Girma <ketema.girma@a2sv.org>
2023-09-20 23:17:10 +0300

    Time: 41 ms (56.49%), Space: 16.5 MB (38.38%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,6 +1,33 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
+       # bit manipulation total num of subset = 2^n
+    
         res = []
+        n = len(nums)
+        for i in range(2**n):
+            subset = []
+            
+            for j in range(n):
+                # check each bit weather it set(1) or not(0)
+                if (i>>j) & 1:
+                    subset.append(nums[j])
+            res.append(subset)
+            
+        return res
+                
+        
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    """
+     res = []
         
         def backtrack(indx, subset):
             if indx >= len(nums):
@@ -18,3 +45,4 @@ class Solution:
         backtrack(0, [])
         
         return res
+    """
