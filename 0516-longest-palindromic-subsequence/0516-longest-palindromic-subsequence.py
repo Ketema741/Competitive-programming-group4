44572e2da454974aa8001393f47b54cfc4344605
Ketema Girma <ketema.girma@a2sv.org>
2023-09-25 23:09:46 +0300

    Time: 2265 ms (8.75%), Space: 538.9 MB (5.01%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.py
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.py
@@ -3,13 +3,13 @@ class Solution:
         cache = {}
         
         def dfs(l, r):
-            if r < 0 or l > len(s)-1:
+            if r < 0 or l > len(s) - 1:
                 return 0
             
-            if (l,r) in cache:
+            if (l, r) in cache:
                 return cache[(l, r)]
 
-            if s[l]==s[r]:
+            if s[l] == s[r]:
                 cache[(l, r)] = 1 + dfs(l + 1, r - 1 )
             else:
                 cache[(l, r)] = max(dfs(l + 1, r), dfs(l, r - 1))
