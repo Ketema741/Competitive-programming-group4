cf3ac040b2de9b48ad7ca8c00c3f86585f243c96
Ketema Girma <ketema.girma@a2sv.org>
2023-10-18 22:57:30 +0300

    Time: 120 ms (52.99%), Space: 17.2 MB (74.07%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -7,7 +7,7 @@ class Solution:
             two_sum = numbers[left] + numbers[right]
             
             if two_sum == target:
-                return [left+1, right+1]
+                return [left + 1, right + 1]
             elif two_sum > target:
                 right -= 1
             else:
