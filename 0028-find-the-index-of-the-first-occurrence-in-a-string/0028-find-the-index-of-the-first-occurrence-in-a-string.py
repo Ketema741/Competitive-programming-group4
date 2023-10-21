b58f13145b03b1bacf4f552fad02aaeaec48a188
Ketema Girma <ketema.girma@a2sv.org>
2023-10-21 13:59:53 +0300

    Time: 37 ms (65.61%), Space: 16.2 MB (77.99%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
@@ -1,29 +1,12 @@
 class Solution:        
     def strStr(self, haystack: str, needle: str) -> int:
-        targetHash = currHash = 0
-        m = len(needle)
-        
-        power = 0
-        for i in range(m - 1, -1, -1):
-            pw = 27**power
-            targetHash += (ord(needle[i]) - ord("a") + 1) * pw
-            
-            if i < len(haystack):
-                currHash +=  (ord(haystack[i]) - ord("a") + 1) * pw
-            power += 1
-            
-        if targetHash == currHash:
-            return 0
-        
+        if len(needle) > len(haystack):
+            return -1
+
         for i in range(len(haystack)):
-            hashVal = ord(haystack[i]) - ord("a") + 1
-            
-            currHash -= hashVal * 27** (m - 1)
-        
-            if i + len(needle)  < len(haystack):
-                currHash = currHash*27 + (ord(haystack[i + len(needle) ]) - ord("a") + 1)
-            
-            if targetHash == currHash:
-                return i+1
-            
+            index = i + len(needle)
+
+            if index <= len(haystack) and needle == haystack[i:index]:
+                return i
+
         return -1
