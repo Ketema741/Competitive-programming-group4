94efa72208d42b8bf5f8cda66f0a754e42511b75
Ketema Girma <ketema.girma@a2sv.org>
2023-10-21 10:10:29 +0300

    Time: 225 ms (41.63%), Space: 24.8 MB (22.74%) - LeetHub

---
+++ 0816-design-hashset/0816-design-hashset.py
@@ -0,0 +1,48 @@
+class ListNode:
+    def __init__(self, key):
+        self.key = key
+        self.next = None
+
+class MyHashSet:
+
+    def __init__(self):
+        self.set = [ListNode(0) for _ in range(10**4)]
+
+    def add(self, key: int) -> None:
+        curr = self.set[key % 10**4]
+
+        while curr.next:
+            if curr.next.key == key:
+                return
+
+            curr = curr.next
+
+        curr.next = ListNode(key)
+
+    def remove(self, key: int) -> None:
+        curr = self.set[key % 10**4]
+        
+        while curr.next:
+            if curr.next.key == key:
+                curr.next = curr.next.next
+                return
+
+            curr = curr.next
+
+
+
+    def contains(self, key: int) -> bool:
+        curr = self.set[key % 10**4]
+        
+        while curr.next:
+            if curr.next.key == key:
+                return True
+                
+            curr = curr.next
+        return False
+
+# Your MyHashSet object will be instantiated and called as such:
+# obj = MyHashSet()
+# obj.add(key)
+# obj.remove(key)
+# param_3 = obj.contains(key)
