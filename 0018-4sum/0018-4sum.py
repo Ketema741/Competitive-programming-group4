aa4421abc546ff4389af375736ca601817e52a56
Ketema Girma <ketema.girma@a2sv.org>
2023-10-22 11:04:50 +0300

    Time: 527 ms (78.99%), Space: 16.4 MB (25.1%) - LeetHub

--- 0018-4sum/0018-4sum.py
+++ 0018-4sum/0018-4sum.py
@@ -1,7 +1,10 @@
 class Solution:
     def fourSum(self, nums, target):
+        nums.sort()
+        results = []
+
         def findNsum(l, r, target, N, result):
-            if r - l + 1 < N or N < 2 or target < nums[l]*N or target > nums[r]*N:  
+            if r - l + 1 < N or N < 2 :  
                 return
                 
             if N == 2: 
@@ -10,19 +13,17 @@ class Solution:
                     if s == target:
                         results.append(result + [nums[l], nums[r]])
                         l += 1
-                        while l < r and nums[l] == nums[l-1]:
+                        while l < r and nums[l] == nums[l - 1]:
                             l += 1
                     elif s < target:
                         l += 1
                     else:
                         r -= 1
             else:
-                for i in range(l, r+1):
-                    if i == l or (i > l and nums[i-1] != nums[i]):
-                        findNsum(i+1, r, target-nums[i], N-1, result+[nums[i]])
+                for i in range(l, r + 1):
+                    if i == l or (i > l and nums[i - 1] != nums[i]):
+                        findNsum(i + 1, r, target - nums[i], N - 1, result + [nums[i]])
 
-        nums.sort()
-        results = []
         findNsum(0, len(nums) - 1, target, 4, [])
 
         return results
