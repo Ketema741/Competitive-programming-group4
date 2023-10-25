3c6766d6750c333be1df9da88171e9b4cf9289d7
Ketema Girma <ketema.girma@a2sv.org>
2023-10-25 22:48:56 +0300

    Time: 105 ms (61.95%), Space: 17.1 MB (61.56%) - LeetHub

---
+++ 0772-construct-quad-tree/0772-construct-quad-tree.py
@@ -0,0 +1,35 @@
+"""
+# Definition for a QuadTree node.
+class Node:
+    def __init__(self, val, isLeaf, topLeft, topRight, bottomLeft, bottomRight):
+        self.val = val
+        self.isLeaf = isLeaf
+        self.topLeft = topLeft
+        self.topRight = topRight
+        self.bottomLeft = bottomLeft
+        self.bottomRight = bottomRight
+"""
+
+class Solution:
+    def construct(self, grid: List[List[int]]) -> 'Node':
+        def dfs(n, r, c):
+            allSame = True
+
+            for i in range(n):
+                for j in range(n):
+                    if grid[r][c] != grid[r + i][c + j]:
+                        allSame = False
+                        break
+            if allSame:
+                return Node(grid[r][c], True)
+
+            n = n // 2
+
+            topLeft = dfs(n, r, c)
+            topRight = dfs(n, r, c + n)
+            bottomLeft = dfs(n, r + n, c)
+            bottomRight = dfs(n, r + n, c + n)
+        
+            return Node(0, False, topLeft, topRight, bottomLeft, bottomRight)
+
+        return dfs(len(grid), 0, 0)
