d1e342c3794c4ef2fc218ece24374a8175fabbbc
Ketema Girma <ketema.girma@a2sv.org>
2023-11-01 16:05:07 +0300

    Time: 57 ms (22%), Space: 21.6 MB (20.75%) - LeetHub

---
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -0,0 +1,23 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        cache = {}
+
+        def dfs(i, buying):
+            if i >= len(prices):
+                return 0
+
+            if (i, buying) in cache:
+                return cache[(i, buying)]
+
+            cooldown = dfs(i + 1, buying)
+
+            if buying:
+                buy = dfs(i + 1, not buying) - prices[i]
+                cache[(i, buying)] = max(buy, cooldown)
+            else:
+                sell = dfs(i + 2, not buying) + prices[i]
+                cache[(i, buying)] = max(sell, cooldown)
+
+            return cache[(i, buying)]
+
+        return dfs(0, True)
