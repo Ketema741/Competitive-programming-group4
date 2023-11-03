d33c74ffaf73a8386f5852170345078128fc4d44
Ketema Girma <ketema.girma@a2sv.org>
2023-11-03 10:05:26 +0300

    Added solution for N-th Tribonacci Number

--- aftercamp/week1/n-th-tribonacci-number.py
+++ aftercamp/week1/n-th-tribonacci-number.py
@@ -1,9 +1,9 @@
 class Solution:
     def tribonacci(self, n: int) -> int:
         t = [0, 1, 1]
-        
-        if n == 0:
-            return t[0]
+
+        if n < 3:
+            return t[n]
 
         for _ in range(3, n + 1):
             t[0], t[1], t[2] = t[1], t[2], sum(t)
