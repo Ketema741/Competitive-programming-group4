2cc66471102b40c27e1209abe4c2f61c20b92e7a
Ketema Girma <ketema.girma@a2sv.org>
2023-11-03 21:40:54 +0300

    Added solution for K Closest Points to Origin

--- aftercamp/week1/k-closest-points-to-origin.py
+++ aftercamp/week1/k-closest-points-to-origin.py
@@ -1,14 +1,15 @@
 class Solution(object):
     def kClosest(self, points, k):
-        maxHeap = []
         res = []
 
-        for x, y in points:
-            distance = x**2 + y**2
-            heapq.heappush(maxHeap, (distance, [x, y]))
+        for i, coord in enumerate(points):
+            x, y = coord
+            points[i] = (x**2 + y**2, [x, y])
+
+        heapq.heapify(points)
 
         while len(res) < k:
-            distance, coord = heapq.heappop(maxHeap)
+            distance, coord = heapq.heappop(points)
             res.append(coord)
 
         return res
