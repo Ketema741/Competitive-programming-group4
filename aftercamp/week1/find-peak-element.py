6907c656ab25b9f49ee4061c810ca0a0227e2d79
Ketema Girma <ketema.girma@a2sv.org>
2023-11-03 09:48:50 +0300

    Added solution for Find Peak Element

---
+++ aftercamp/week1/find-peak-element.py
@@ -0,0 +1,18 @@
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
+        res = 0
+        N = len(nums) - 1
+
+        left, right = 0, N
+
+        while left <= right:
+            mid = left + (right - left)//2
+
+
+            if mid > 0 and nums[mid] < nums[mid - 1]:
+                right = mid - 1
+            elif mid < N and nums[mid] < nums[mid + 1]:
+                left = mid + 1
+            else:
+                return mid
+            
