111a8c1a19f150ad90fbf621d984fec15c69367a
Ketema Girma <ketema.girma@a2sv.org>
2023-11-03 13:47:55 +0300

    Time: 101 ms (60.49%), Space: 16.2 MB (82.23%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.py
+++ 0647-palindromic-substrings/0647-palindromic-substrings.py
@@ -2,16 +2,17 @@ class Solution:
     def countSubstrings(self, s: str) -> int:
         res = 0
 
-        for i in range(len(s)):
-            res += self.countPali(s, i, i)
-            res += self.countPali(s, i, i + 1)
-            
-        return res
+        def countPal(l, r):
+            count = 0
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                count += 1
+                l -= 1
+                r += 1
 
-    def countPali(self, s, l, r):
-        res = 0
-        while l >= 0 and r < len(s) and s[l] == s[r]:
-            res += 1
-            l -= 1
-            r += 1
+            return count
+
+        for i in range(len(s)):
+            res += countPal(i, i)
+            res += countPal(i , i + 1)
+        
         return res
