559df680908a5a1b01cbf77469cb1c5b2a538d9e
Ketema Girma <ketema.girma@a2sv.org>
2023-11-05 17:51:55 +0300

    Time: 325 ms (51.22%), Space: 32 MB (72.36%) - LeetHub

---
+++ 1499-maximum-performance-of-a-team/1499-maximum-performance-of-a-team.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maxPerformance(self, n: int, speed: List[int], efficiency: List[int], k: int) -> int:
+        pairs = [(eff, spd) for eff, spd in zip(efficiency, speed)]
+        pairs.sort(reverse=True)
+        mod = 10 ** 9 + 7
+        min_heap = []
+        res, tot_speed = 0, 0
+
+        for eff, spd in pairs:
+            if len(min_heap) == k:
+               val = heapq.heappop(min_heap)
+               tot_speed -= val
+
+            tot_speed += spd
+            res = max(res, tot_speed * eff)
+            heapq.heappush(min_heap, spd)
+        
+        return res % mod
