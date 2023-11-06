29c32c2d684a65437111c6b3b00c4552dc121998
Ketema Girma <ketema.girma@a2sv.org>
2023-11-06 23:08:27 +0300

    Time: 76 ms (71.4%), Space: 20.1 MB (25.13%) - LeetHub

--- 0303-range-sum-query-immutable/0303-range-sum-query-immutable.py
+++ 0303-range-sum-query-immutable/0303-range-sum-query-immutable.py
@@ -10,4 +10,4 @@ class NumArray:
     def sumRange(self, left: int, right: int) -> int:
         r = self.prefix[right] 
         l = self.prefix[left - 1] if left > 0 else 0
-        return r - l
+        return r - l 
