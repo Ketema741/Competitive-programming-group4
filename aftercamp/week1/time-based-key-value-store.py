af176aeb042e1a14687a60bf1d2813fde65404f4
Ketema Girma <ketema.girma@a2sv.org>
2023-11-07 19:27:25 +0300

    Added solution for Time Based Key-Value Store

---
+++ aftercamp/week1/time-based-key-value-store.py
@@ -0,0 +1,31 @@
+class TimeMap:
+
+    def __init__(self):
+        self.hash_map = defaultdict(list)
+        
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        self.hash_map[key].append([value, timestamp])
+        
+
+    def get(self, key: str, timestamp: int) -> str:
+        res = ""
+        values = self.hash_map.get(key, [])
+
+        left, right = 0, len(values) - 1
+        while left <= right:
+            mid = (left + right) // 2
+            if values[mid][1] <= timestamp:
+                res = values[mid][0]
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return res
+        
+
+
+# Your TimeMap object will be instantiated and called as such:
+# obj = TimeMap()
+# obj.set(key,value,timestamp)
+# param_2 = obj.get(key,timestamp)
