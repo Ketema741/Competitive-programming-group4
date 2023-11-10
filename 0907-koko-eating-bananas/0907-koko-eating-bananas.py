50b8cb0b5577552c6ed4ebe86bfcba1df1dcbe72
Ketema Girma <ketema.girma@a2sv.org>
2023-11-10 17:30:34 +0300

    Time: 350 ms (69.68%), Space: 18 MB (16.16%) - LeetHub

---
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.py
@@ -0,0 +1,20 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        
+        def canEatAll(k, hrs_left):
+            for pile in piles:
+                hrs_left -= math.ceil(pile/k)
+            
+            return hrs_left >= 0
+
+        left, right = 1, max(piles)
+        
+        while left <= right:
+            mid = (left + right) // 2
+            
+            if canEatAll(mid, h):
+                right = mid - 1
+            else:
+                left = mid + 1
+                
+        return left
