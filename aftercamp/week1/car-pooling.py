8019b24715ae8ec6618caaa48759761e9cc9aa9b
Ketema Girma <ketema.girma@a2sv.org>
2023-11-17 21:32:21 +0300

    Added solution for Car Pooling

---
+++ aftercamp/week1/car-pooling.py
@@ -0,0 +1,18 @@
+class Solution:
+    def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
+        trips.sort(key=lambda t:t[1])
+        minHeap = []  
+        currPass = 0
+
+        for numPass, start, end in trips:
+            while minHeap and minHeap[0][0] <= start:
+                currPass -= heapq.heappop(minHeap)[1]
+
+            currPass += numPass
+
+            if currPass > capacity:
+                return False
+            
+            heapq.heappush(minHeap, (end, numPass))
+
+        return True
