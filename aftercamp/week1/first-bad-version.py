162e947643cae8bc642e35e7551feaf9cc7e8b48
Ketema Girma <ketema.girma@a2sv.org>
2023-11-21 21:40:48 +0300

    Added solution for First Bad Version

---
+++ aftercamp/week1/first-bad-version.py
@@ -0,0 +1,18 @@
+# The isBadVersion API is already defined for you.
+# def isBadVersion(version: int) -> bool:
+
+class Solution:
+    def firstBadVersion(self, n: int) -> int:
+        left, right = 1, n
+        
+        while left < right:
+            mid = (left + right) // 2
+            
+            res = isBadVersion(mid)
+            
+            if res:
+                right = mid
+            else:
+                left = mid + 1
+                
+        return left
