4e7ac23cf3e1d57453c6217e157337278141acf8
Ketema Girma <ketema.girma@a2sv.org>
2023-11-21 11:16:05 +0300

    Added solution for Single-Threaded CPU

--- aftercamp/week1/single-threaded-cpu.py
+++ aftercamp/week1/single-threaded-cpu.py
@@ -1,22 +1,23 @@
 class Solution:
     def getOrder(self, tasks: List[List[int]]) -> List[int]:
-        for i, task in enumerate(tasks):
+        for i, task in enumerate(tasks): #O(N)
             task.append(i)
 
         tasks.sort()
-        res, minHeap = [], []
+        res, available_tasks = [], []
         i, time = 0, tasks[0][0]
 
-        while minHeap or i < len(tasks):
+        
+        while available_tasks or i < len(tasks): # O(N)
             while i < len(tasks) and time >= tasks[i][0]:
-                heapq.heappush(minHeap, [tasks[i][1], tasks[i][2]])
+                heapq.heappush(available_tasks, [tasks[i][1], tasks[i][2]]) # O(log(N))
                 i += 1
-
-            if not minHeap:
-                time = tasks[i][0]
+            
+            if not available_tasks:
+                time = tasks[i][0] # skip idle time
             else:
-                procTime, index = heapq.heappop(minHeap)
-                time += procTime
+                proces_time, index = heapq.heappop(available_tasks)
+                time += proces_time
                 res.append(index)
 
         return res
