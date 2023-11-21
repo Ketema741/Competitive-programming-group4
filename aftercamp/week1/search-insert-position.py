9983658e1875fedc7081f9c2b1830cebd1eb9145
Ketema Girma <ketema.girma@a2sv.org>
2023-11-21 21:41:04 +0300

    Added solution for Search Insert Position

---
+++ aftercamp/week1/search-insert-position.py
@@ -0,0 +1,14 @@
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        left = 0
+        right = len(nums)
+        
+        while left < right:
+            mid = (left + right) // 2
+
+            if nums[mid] >= target:
+                right = mid
+            else:
+                left = mid + 1
+
+        return left
