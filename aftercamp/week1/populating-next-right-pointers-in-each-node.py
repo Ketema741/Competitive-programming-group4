1c302b037d2fa53900e13ea5e1ce62db3a9e0291
Ketema Girma <ketema.girma@a2sv.org>
2023-11-22 15:13:51 +0300

    Added solution for Populating Next Right Pointers in Each Node

---
+++ aftercamp/week1/populating-next-right-pointers-in-each-node.py
@@ -0,0 +1,27 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+
+class Solution:
+    def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
+        curr, nxt = root, root.left if root else None
+
+        while curr and nxt:
+            curr.left.next = curr.right
+
+            if curr.next:
+                curr.right.next = curr.next.left
+
+            curr = curr.next
+
+            if not curr:
+                curr = nxt
+                nxt = nxt.left
+
+        return root
