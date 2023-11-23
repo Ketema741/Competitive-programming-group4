8a9396e5ca6797606ece8d00c34f955da4d3fb62
Ketema Girma <ketema.girma@a2sv.org>
2023-11-23 12:04:55 +0300

    find k

---
+++ codeforce/Contest/contest 5/D_Find_K.py
@@ -0,0 +1,35 @@
+def solve():
+    n , k = map(int , input().split())
+    nums = list(map(int, input().split()))
+    
+    left = 0
+    right = 1
+    found = False
+    nums.sort()
+    while right < n:
+        difference = nums[right] - nums[left]
+        if difference == k:
+             found = True
+             break
+        elif difference > k:
+              left += 1
+        else:
+              right += 1
+    
+    if found:
+        print("YES")
+    else:
+        print("NO")
+        
+def main():
+    t = int(input())
+    
+    for _ in range(t):
+        solve()
+
+main()
+
+"""
+1 1 3 4 9
+"""
+
