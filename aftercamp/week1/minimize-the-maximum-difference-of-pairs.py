04711538f9dcfc36cb16d2bfa65b68d1c1cf4aae
Ketema Girma <ketema.girma@a2sv.org>
2023-12-02 15:45:50 +0300

    Added solution for Minimize the Maximum Difference of Pairs

--- aftercamp/week1/minimize-the-maximum-difference-of-pairs.py
+++ aftercamp/week1/minimize-the-maximum-difference-of-pairs.py
@@ -1,34 +1,32 @@
 class Solution:
     def minimizeMax(self, nums: List[int], p: int) -> int:
-        def isValid(threshold):
-            i, count = 0, 0
-
-            while i < len(nums) - 1:
-                if (nums[i + 1] - nums[i]) <= threshold:
-                    i += 2
-                    count += 1
-                else:
-                    i += 1
-                
-                if count == p:
-                    return True
-            return False
-        
-
         left, right = 0, 10**9
         res = 0
-        
-
         nums.sort()
 
         while left <= right:
             mid = left + (right - left)//2
 
-            if isValid(mid):
+            if self.isValid(mid, nums, p):
                 right = mid - 1
                 res = mid
             else:
                 left = mid + 1
 
         return res
+
+    def isValid(self, threshold, nums , p):
+        i, count = 0, 0
+
+        while i < len(nums) - 1:
+            if (nums[i + 1] - nums[i]) <= threshold:
+                i += 2
+                count += 1
+            else:
+                i += 1
+            
+            if count == p:
+                return True
+        return False
+
                 
