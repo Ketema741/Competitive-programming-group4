d135f45fab6eb6bfeebb9470d4d707068d4bf6af
Ketema Girma <ketema.girma@a2sv.org>
2023-12-02 11:38:01 +0300

    Added solution for Arranging Coins

--- aftercamp/week1/arranging-coins.py
+++ aftercamp/week1/arranging-coins.py
@@ -1,21 +1,19 @@
 class Solution:
     def arrangeCoins(self, n: int) -> int:
         res = 0
-        
         left, right = 0, n
         
         while left <= right:
-            
             mid = left + (right - left)//2
             
-            coins = mid*(mid + 1)/2
-            
-            if coins <= n:
-                res = max(res, mid)
+            if self.canBuild(mid, n):
                 left = mid + 1
             else:
                 right = mid - 1
                 
-        return res
+        return right
+
+    def canBuild(self, coins, n):
+            return coins*(coins + 1)/2 <= n
             
             
