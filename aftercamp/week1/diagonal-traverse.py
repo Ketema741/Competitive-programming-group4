fa90fb5fbf67cf22755e86ce6cdbf0af9c8cd41f
Ketema Girma <ketemagirma15@gmail.com>
2023-12-28 18:33:17 +0300

    Added solution for Diagonal Traverse

---
+++ aftercamp/week1/diagonal-traverse.py
@@ -0,0 +1,29 @@
+class Solution:
+    def findDiagonalOrder(self, mat: List[List[int]]) -> List[int]:
+        hash_map = defaultdict(list)
+        res = []
+        for row in range(len(mat)):
+            for col in range(len(mat[0])):
+                hash_map[row + col].append(mat[row][col])
+      
+        for key, val in hash_map.items():
+            if key%2 == 0:
+                res.extend(val[::-1])
+            else:
+                res.extend(val)
+        return res
+
+
+
+"""
+(0, 0)
+      0 1 2
+    0 1 2 3 
+    1 4 5 6
+    2 7 8 9
+
+
+
+"""
+        
+        
